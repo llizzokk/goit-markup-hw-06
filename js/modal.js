84564b754a34f64de2f62d9e2382dd346df26256
Liza <lizaapostol378@gmail.com>
2024-08-01 17:41:34 +0300

    update mob menu

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,17 @@
     refs.modal.classList.toggle("is-open");
   }
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+  }
+})();
